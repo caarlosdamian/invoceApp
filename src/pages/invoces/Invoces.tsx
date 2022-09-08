7eb13879d1e37c68834c4291c6821f5eb0d6edc7
caarlosdamian <dapecarlos@gmail.com>
2022-09-07 23:36:56 -0500

    Added dark theme on labels

--- src/pages/invoces/Invoces.tsx
+++ src/pages/invoces/Invoces.tsx
@@ -2,20 +2,23 @@ import React, { useState } from "react";
 import "./Invoces.scss";
 import arrow_down from "../../assets/icon-arrow-down.svg";
 import plus from "../../assets/icon-plus.svg";
+import { useSelector } from "react-redux";
 
 export const Invoces = () => {
   const [open, setOpen] = useState(false);
+  const { dark } = useSelector((state: any) => state.theme);
+
   return (
     <div className="invoces__container">
       <div className="invoces__container--top--left">
-        <h1 className="invoces__container--top--header">Invoices</h1>
-        <span className="invoces__container--top--subtitle">
+        <h1 className={`invoces__container--top--header ${dark}`}>Invoices</h1>
+        <span className={`invoces__container--top--subtitle ${dark}`}>
           There are 4 pending invoices
         </span>
       </div>
       <div className="invoces__container--middle">
         <span
-          className="invoces__container--middle-text"
+          className={`invoces__container--middle-text ${dark}`}
           onClick={() => setOpen(!open)}
         >
           Filter by status
