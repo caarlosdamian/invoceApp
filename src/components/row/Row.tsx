bbabda00b9739fcade86146ad66e75af36502ae4
caarlosdamian <dapecarlos@gmail.com>
2022-09-09 00:36:33 -0500

    added Tip component WIP

---
+++ src/components/row/Row.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Row.scss'
+
+export const Row = () => {
+  return (
+    <div className='row__container'>Row</div>
+  )
+}
---
+++ src/components/tip/Tip.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import "./Tip.scss";
+
+interface TipProps {
+  label: "Pending" | "Paid" | "Draft";
+  type: "pending" | "paid" | "draft";
+}
+export const Tip = ({ label, type }: TipProps) => {
+  const { dark } = useSelector((state: any) => state.theme);
+
+  return (
+    <div className={`tip__container ${type} ${dark}`}>
+      <div className={`tip__circle ${type} ${dark}`}></div>
+      <span className={`tip__label ${type} ${dark}`}>{label}</span>
+    </div>
+  );
+};
