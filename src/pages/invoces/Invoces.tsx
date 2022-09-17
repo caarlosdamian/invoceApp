f55059220b35842d99263b632d4860dd0b72e891
carlos eduardo damian perez <dapecarlos@gmail.com>
2022-09-17 18:26:53 -0500

    feat: Added dark theme to filterbox

--- src/pages/invoces/Invoces.tsx
+++ src/pages/invoces/Invoces.tsx
@@ -1,20 +1,23 @@
 import { useMemo, useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import arrow_down from "../../assets/icon-arrow-down.svg";
 import plus from "../../assets/icon-plus.svg";
 import { MiniBox, Row } from "../../components";
 import { useWindowSize } from "../../hooks/useWindowSize";
 import { RowInfo } from "../../interfaces";
-import { selectPostsByUser } from "../../redux/slices/InvoceSlice";
+import {
+  selectPostsByUser,
+  showFilterToggle,
+} from "../../redux/slices/InvoceSlice";
 
 import "./Invoces.scss";
 
 export const Invoces = () => {
-  const [open, setOpen] = useState(false);
+  const dispatch = useDispatch();
   const { width } = useWindowSize();
   const { theme, invoice } = useSelector((state: any) => state);
-  const { invoces, selectedFilter } = invoice;
+  const { invoces, selectedFilter, showFilter } = invoice;
   const { dark } = theme;
   const invoceLength = selectPostsByUser(invoces);
 
@@ -33,7 +36,7 @@ export const Invoces = () => {
 
   return (
     <div className="invoces">
-      {open && <MiniBox /> }
+      {showFilter && <MiniBox /> }
       
       <div className="invoces__container">
         <div className="invoces__container--top--left">
@@ -49,14 +52,17 @@ export const Invoces = () => {
         <div className="invoces__container--middle">
           <span
             className={`invoces__container--middle-text ${dark}`}
-            onClick={() => setOpen(!open)}
+            onClick={() => dispatch(showFilterToggle())}
           >
             {width <= 430 ? "Filter" : "Filter by status"}
           </span>
           <img
+            onClick={() => dispatch(showFilterToggle())}
             src={arrow_down}
             alt={arrow_down}
-            className={`invoces__container--middle-img ${open ? "open" : ""}`}
+            className={`invoces__container--middle-img ${
+              showFilter ? "open" : ""
+            }`}
           />
         </div>
         <div className="invoces__container--top--right">
