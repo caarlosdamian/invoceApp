92e80a0fb94dc51ba512d56065d3a548ea3c20d8
caarlosdamian <dapecarlos@gmail.com>
2022-10-11 22:37:29 -0500

    work in progress

--- src/pages/Details/Details.tsx
+++ src/pages/Details/Details.tsx
@@ -1,3 +1,4 @@
+import { useSelector } from "react-redux";
 import { Link, useLocation } from "react-router-dom";
 import arrow_down from "../../assets/icon-arrow-down.svg";
 import { Button } from "../../components";
@@ -8,6 +9,7 @@ import "./Details.scss";
 
 export const Details = () => {
   const { state } = useLocation();
+  const { dark } = useSelector((state: any) => state.theme);
   const { data } = state;
   const { status } = data;
   const capitalLetter = useCapitalize(status);
@@ -18,7 +20,7 @@ export const Details = () => {
         <img src={arrow_down} alt="arrrow-left" className="arrow-left" />
         <span className="details__container-top-label">Go Back</span>
       </Link>
-      <div className="details__container-middle">
+      <div className={`details__container-middle ${dark}`}>
         <div className="details__container-middle-left">
           <span className="details__container-middle-span">Status</span>
           <Tip type={status} label={capitalLetter} />
@@ -29,7 +31,15 @@ export const Details = () => {
           <Button size="medium" theme="default__purple" label="Mark as Paid" />
         </div>
       </div>
-      <div className="details__container-main">Details</div>
+      <div className={`details__container-main ${dark}`}>
+        <div className="details__container-main-grid">
+          <div className="details__top-left">Test</div>
+          <div className="details__top-right">test</div>
+          <div className="details__middle-left">test</div>
+          <div className="details__middle-middle">test</div>
+          <div className="details__middle-right">test</div>
+        </div>
+      </div>
     </div>
   );
 };
