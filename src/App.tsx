82e0d9af1ca8a2d169b32be07a11d2d78172e253
caarlosdamian <dapecarlos@gmail.com>
2022-10-24 22:32:15 -0500

    feat:(render): added modal and form component on top level app component;

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Sidebar } from "./components/";
+import { Sidebar,Modal,Form } from "./components/";
 import { CustomRoutes } from "./routes/CustomRoutes";
 import "./App.scss";
-import { Modal } from "./components/Modal/Modal";
 import { showModal } from "./redux/slices/ModalSlice";
 
 const App = () => {
- const {
-  theme: { dark },
-  modal,
- } = useSelector((state: any) => state);
- const show = showModal(modal);
- return (
-  <main className={`main__container ${dark}`} data-testid="main__container">
-    {show && <Modal />}
-   <Sidebar />
-   <div className="main__content">
-    <CustomRoutes />
-   </div>
-  </main>
- );
+  const {
+    theme: { dark },
+    modal,
+  } = useSelector((state: any) => state);
+  const show = showModal(modal);
+  return (
+    <main className={`main__container ${dark}`} data-testid="main__container">
+      {show && (
+        <Modal>
+          <Form />
+        </Modal>
+      )}
+      <Sidebar />
+      <div className="main__content">
+        <CustomRoutes />
+      </div>
+    </main>
+  );
 };
 
 export default App;
