938c3b234e877d75ecf931f6b919431eee645495
caarlosdamian <dapecarlos@gmail.com>
2022-10-24 22:31:19 -0500

    chore(imports): update component imports

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,7 @@ export { Sidebar } from "./sidebar/Sidebar";
 export { Button } from "./button/Button";
 export { Table } from "./table/Table";
 export { Tip } from "./tip/Tip";
+export { TextInput } from "./textInput/TextInput";
+export { Modal } from "./Modal/Modal";
+export { Form } from "./form/Form";
+
