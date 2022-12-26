5f2aca7f53e9bf25d3a8498e76745565f45e1932
caarlosdamian <dapecarlos@gmail.com>
2022-12-25 22:42:19 -0600

    feat(modals): added new modals

---
+++ src/components/formDelete/FormDelete.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Modal } from "../Modal/Modal";
+
+export const FormDelete = () => {
+
+    // dispatch(deleteInvoce(data.id));
+  return (
+      <span>Testing</span>
+  );
+};
---
+++ src/components/formMobile/FormMobile.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const FormMobile = () => {
+  return (
+    <div>FormMobile</div>
+  )
+}
