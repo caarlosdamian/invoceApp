1c03229e3b13a356e75b94954043706f3b954d31
caarlosdamian <dapecarlos@gmail.com>
2022-12-25 22:44:21 -0600

    feat(modals): added new modal functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,37 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Sidebar,Modal,Form } from "./components/";
+import { Sidebar, Modal, Form } from "./components/";
 import { CustomRoutes } from "./routes/CustomRoutes";
 import "./App.scss";
 import { showModal } from "./redux/slices/ModalSlice";
+import { FormDelete } from "./components/formDelete/FormDelete";
+import { FormMobile } from "./components/formMobile/FormMobile";
+// Pending move to components index
 
 const App = () => {
   const {
     theme: { dark },
+    modal: { type },
     modal,
   } = useSelector((state: any) => state);
   const show = showModal(modal);
   return (
     <main className={`main__container ${dark}`} data-testid="main__container">
-      {show && (
+      {show && type === "form" ? (
         <Modal>
           <Form />
         </Modal>
+      ) : show && type === "delete" ? (
+        <Modal>
+          <FormDelete />
+        </Modal>
+      ) : (
+        show &&
+        type === "form-mobile" && (
+          <Modal>
+            <FormMobile />
+          </Modal>
+        )
       )}
       <Sidebar />
       <div className="main__content">
